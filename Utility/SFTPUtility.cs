d1c4ecca40d71aad39d23c191bb8d5c02a01df3a
Shahriar Hossain <shahriar_cse@live.com>
2016-08-05 17:21:36 +0600

    SFTP helper
    Helper class to do operation in SFTP using Renci.SshNet

---
+++ Utility/SFTPUtility.cs
@@ -0,0 +1,79 @@
+﻿using UtilityHouse.Entity;
+using Renci.SshNet;
+using System;
+using System.IO;
+
+namespace GTW.Sync.Service.Helper.SFTP
+{
+    public class SFTPUtility
+    {
+
+        private static SFTPUtility instance = null;
+        private static ConnectionInfo conncectionInfo = null;
+
+        private SFTPUtility()
+        {
+            conncectionInfo = new ConnectionInfo(ApplicationConstants.SFTPHost, ApplicationConstants.SFTPPort, ApplicationConstants.SFTPUsername,
+            new AuthenticationMethod[]{
+                new PasswordAuthenticationMethod(ApplicationConstants.SFTPUsername, ApplicationConstants.SFTPPassword)
+            });
+        }
+
+        public static SFTPUtility Instance
+        {
+            get
+            {
+                if (instance == null || conncectionInfo == null)
+                {
+                    instance = new SFTPUtility();
+                }
+                return instance;
+            }
+        }
+
+        public bool UploadFile(Stream fileStream, string filename)
+        {
+            try
+            {
+                using (var sftp = new SftpClient(conncectionInfo))
+                {
+                    sftp.Connect();
+
+                    sftp.UploadFile(fileStream, ApplicationConstants.SFTPUploadFileLocation + filename);
+
+                    sftp.Disconnect();
+                }
+                return true;
+            }
+            
+            catch (Exception ex)
+            {
+                return false;
+            }
+        }
+
+        public Stream DownloadFile(string fileName)
+        {
+            try
+            {
+                using (var sftp = new SftpClient(conncectionInfo))
+                {
+                    sftp.Connect();
+
+                    //fileName = "readme.txt";
+
+                    Stream fileToSave = File.OpenWrite(fileName);
+                    sftp.DownloadFile(ApplicationConstants.SFTPDownloadFileLocation + fileName, fileToSave);
+
+                    sftp.Disconnect();
+
+                    return fileToSave;
+                }
+            }
+            catch(Exception ex)
+            {
+                return null;
+            }
+        }
+    }
+}
