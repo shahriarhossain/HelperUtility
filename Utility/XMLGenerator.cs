364ba9402aa655b2544e6e5cf802a215fe8259e8
Shahriar Hossain <shahriar_cse@live.com>
2018-09-11 16:06:57 +0600

    XML generation from C# object
    public class Employee
    {
            [XmlElement("FN")]
            public string FirstName { get; set; }
            [XmlElement("LN")]
            public string LastName { get; set; }
            public FamilyTitleAttribute FamilyTitle { get; set; }
    }
    
    public class FamilyTitleAttribute
    {
            [XmlAttribute]
            public string title { get; set; }
    }
    
    public class Root
    {
            public string Company { get; set; }
            public List<Employee> empList { get; set; }
            public string Year { get; set; }
    }
    
    Call the XMLGenerator.GetXMLFromObject() method and pass the object.
    List<Employee> empList = new List<Employee>()
    {
              new Employee() { FirstName = "Code", LastName = "Handbook" , FamilyTitle = new FamilyTitleAttribute{ title ="Alba" }  },
              new Employee() { FirstName = "HERE", LastName = "Handbook", FamilyTitle= new FamilyTitleAttribute{ title ="Hossain" }  }
    };
    Root r = new Root()
    {
              Company = "Humana",
              Year = "2018",
               empList = empList
    };
    XMLGenerator.GetXMLFromObject(r);

---
+++ Utility/XMLGenerator.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Xml.Serialization;
+using System.IO;
+using System.Xml;
+
+namespace PaySpan.Sync.Service.Helper
+{
+    public class XMLGenerator
+    {
+        public static string GetXMLFromObject(object o)
+        {
+            StringWriter sw = new StringWriter();
+            XmlTextWriter tw = null;
+            XmlSerializerNamespaces ns = new XmlSerializerNamespaces();
+            ns.Add("", "");
+
+            try
+            {
+                XmlSerializer serializer = new XmlSerializer(o.GetType());
+                
+                tw = new XmlTextWriter(sw);
+  
+                serializer.Serialize(tw, o, ns);
+            }
+            catch (Exception ex)
+            {
+                //Handle Exception Code
+            }
+            finally
+            {
+                sw.Close();
+                if (tw != null)
+                {
+                    tw.Close();
+                }
+            }
+            return sw.ToString();
+        }
+
+    }
+}
