17c6f43c20bb186e8f966d2fca563672ccf86265
Shahriar Hossain <shahriar_cse@live.com>
2019-01-29 20:26:07 +0600

    Contains method for string of array

---
+++ Utility/Extension.cs
@@ -0,0 +1,18 @@
+﻿using System.Linq;
+
+namespace SecretBook.Helper
+{
+    public static class Extension
+    {
+        public static bool Contains(this string value, params string[] values)
+        {
+            if (values.Length <= 0)
+                return true;
+            else
+            {
+                values = values.Select(x => x.ToLower().Trim()).ToArray();
+                return values.Contains(value);
+            }
+        }
+    }
+}
