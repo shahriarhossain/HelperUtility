b130ce1208acec16262cb67ca631caac994918bd
Shahriar Hossain <shahriar_cse@live.com>
2019-07-17 16:27:35 +0600

    Add files via upload

---
+++ Utility/ConfigurationSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+
+namespace SecretBook.Helper
+{
+    public static class ConfigurationSettings
+    {
+        public static bool IsProductionEnv => Convert.ToBoolean(ConfigurationManager.AppSettings.Get("ProdEnv"));
+        public static string GetEndPointUrl => ConfigurationManager.AppSettings.Get("Url");
+        public static List<string> GetAllowedDomains => ConfigurationManager.AppSettings.Get("AllowDomains").Split(',').Select(x => x.Trim()).ToList();
+    }
+}
